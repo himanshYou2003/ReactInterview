0271289e1887533999ab46dc4f7e3ab02b095924
himanshu <himanshukumar935437#gmail.com>
2024-10-08 23:48:10 +0530

    production ready

--- Basic/App.js
+++ Basic/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 // const heading =React.createElement("h1",{id:"heading"},"Hi Himanshu Im Rendering  !");
 const parent = React.createElement("div",{id:"parent"},
     React.createElement("div",{id:"child"},
         [
-            React.createElement("h1",{},"Im h1 Tag !"),
+            React.createElement("h1",{},"Im h1 Tag ,Nope"),
             React.createElement("h2",{},"Im h2 Tag !")
         ]
     )
