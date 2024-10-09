a440225c46c83b308e586a2db4e412d12f6eda05
himanshu <himanshukumar935437#gmail.com>
2024-10-09 22:53:26 +0530

    jsx

--- Basic/App.js
+++ Basic/App.js
@@ -1,16 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// const heading =React.createElement("h1",{id:"heading"},"Hi Himanshu Im Rendering  !");
-const parent = React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},
-        [
-            React.createElement("h1",{},"Im h1 Tag ,Nope"),
-            React.createElement("h2",{},"Im h2 Tag !")
-        ]
-    )
-)
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
 
-root.render(parent);
+
+// const heading = React.createElement("h1",{id:"heading"},"Hi Himanshu");
+const jsxheading =<h1>Hi Himanshu It's me !</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxheading);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const heading =React.createElement("h1",{id:"heading"},"Hi Himanshu Im Rendering  !");
+// const parent = React.createElement("div",{id:"parent"},
+//     React.createElement("div",{id:"child"},
+//         [
+//             React.createElement("h1",{},"Im h1 Tag ,Nope"),
+//             React.createElement("h2",{},"Im h2 Tag !")
+//         ]
+//     )
+// )
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// // root.render(heading);
+
+// root.render(parent);
